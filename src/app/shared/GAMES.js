c98865323b1ac94de56d853e775e0759c1ca99b3
Sam Rider <ridersam1023@gmail.com>
2023-08-25 12:53:23 -0500

    MVP Arrays DONE

--- src/app/shared/GAMES.js
+++ src/app/shared/GAMES.js
@@ -1,14 +1,33 @@
-import shooter from '../assets/images/shooter.png'
-
+import shooter from '../assets/img/shootergame.png';
+import stray from '../assets/img/stray.jpg';
+import fallFlat from '../assets/img/fallflat.jpg';
 
 export const GAMES = [
     {
         id: 0,
-        name: 'Shooter Game',
+        name: 'Shooter',
         image: shooter,
         released: 2021,
         featured: false,
         description:
             'Protect the world from the crusades of evil underground organizations.'
-    }
+    },
+    {
+        id: 1,
+        name: 'Stray',
+        image: stray,
+        released: 2022,
+        featured: true,
+        description:
+            'Live the life of a small hosue cat in the future. Take a stroll through th streets, or take a nap in the sheets.'
+    },
+    {
+        id: 2,
+        name: 'Human Fall Flat',
+        image: fallFlat,
+        released: 2020,
+        featured: false,
+        description:
+            'Cooperate with friends or explore on your own. Solve each stage of puzzles and experience this dream like world for all it has to offer.'
+    },
 ];
---
+++ src/app/shared/TIERS.js
@@ -0,0 +1,32 @@
+
+
+
+export const TIERS = [
+    {
+        id: 0,
+        name: 'Intro to Gaming',
+        image: shooter,
+        price: "free to subscribe!",
+        featured: false,
+        description:
+            'Subscribe today to be kept up to date with company news and releases. This tier includes access to featured games, and discounts on all other games.'
+    },
+    {
+        id: 1,
+        name: 'Gaming Enthusiast',
+        image: stray,
+        price: "$4.99/month to subscribe!",
+        featured: true,
+        description:
+            'Dive in deeper to what Andiamo has to offer. This tier gives you access to blog comments, featured games, guides and company updates. Discounts to our gaming library is included.'
+    },
+    {
+        id: 2,
+        name: 'True Gamer',
+        image: fallFlat,
+        price: "$14.99/month to subscribe!",
+        featured: false,
+        description:
+            'You made it! Become the gamer you want to be by subscribing tothsi tier. Have the opportunity to submit blog ideas, commetn and share i nthe community, full access to our game libray, and be added to our email newsletter about industry news.'
+    }
+];
